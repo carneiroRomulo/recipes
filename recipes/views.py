b75a72f9db7011092294288c180718881de76456
Romulo Carneiro <romulo.carneirodk@gmail.com>
2022-05-26 09:18:21 -0300

    Add template

--- recipes/views.py
+++ recipes/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import HttpResponse
+from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return HttpResponse('Hello World')
+    return render(request, 'home.html')
