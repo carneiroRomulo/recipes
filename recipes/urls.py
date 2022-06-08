05c1746ff9dd54bd2a415b95130cb0f907edd042
Romulo Carneiro <romulo.carneirodk@gmail.com>
2022-06-08 09:19:11 -0300

    Inject fake data % update urls

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import home
+from . import views
+
+app_name = 'recipes'
 
 urlpatterns = [
-    path('', home),
+    path('', views.home, name="home"),
+    path('recipe/<int:id>/', views.recipe, name="recipe"),
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
 
+from utils.fake_data import make_recipe
+
 # Create your views here.
 def home(request):
-    return render(request, 'pages/home.html')
+    return render(request, 'pages/home.html', context={
+        'recipes': [make_recipe() for _ in range(10)],
+    })
+
+def recipe(request, id):
+    return render(request, 'pages/recipe.html', context={
+        'recipe': make_recipe(),
+        'is_detail_page': True,
+    })
