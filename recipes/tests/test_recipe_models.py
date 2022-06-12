6e6fdfb3510db5288e94aab9237b71cd24736249
Romulo Carneiro <romulo.carneirodk@gmail.com>
2022-06-11 21:51:51 -0300

    Add dependancy coverage & make tests for model category

--- recipes/tests/test_recipe_models.py
+++ recipes/tests/test_recipe_models.py
@@ -44,3 +44,31 @@ class RecipeModelTest(RecipeTestBase):
         self.assertFalse(
             getattr(self.recipe, field),
         )
+
+    def test_recipe_string_representation(self):
+        needed = 'Testing Representation'
+        self.recipe.title = needed
+        self.recipe.full_clean()
+        self.recipe.save()
+        self.assertEqual(
+            str(self.recipe), needed,
+            msg=f'Recipe string representation \
+                must be "{needed}", but "{str(self.recipe)}" was received.'
+        )
+
+
+class RecipeCategoryModelTest(RecipeTestBase):
+    def setUp(self) -> None:
+        self.category = self.make_category(name='Category Testing')
+        return super().setUp()
+
+    def test_recipe_category_model_string_representation_is_name_field(self):
+        self.assertEqual(
+            str(self.category),
+            self.category.name
+        )
+
+    def test_repice_category_model_name_max_length_is_65_chars(self):
+        self.category.name = 'A' * 66
+        with self.assertRaises(ValidationError):
+            self.category.full_clean()
