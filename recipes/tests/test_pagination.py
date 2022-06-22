5c54b78d3b033e74e57d5221f7ae53bde52f8a25
Romulo Carneiro <romulo.carneirodk@gmail.com>
2022-06-22 07:59:23 -0300

    test: Update pagination test to receive a dynamic range of pages

--- recipes/tests/test_pagination.py
+++ recipes/tests/test_pagination.py
@@ -4,10 +4,34 @@ from utils.pagination import make_pagination_range
 
 
 class PaginationTest(TestCase):
-    def test_make_pagination_range_returns_pagination_range(self):
+    def test_make_pagination_range_returns_correct_pagination_range(self):
+        """When current page is bigger than (page_quantity / 2)
+        pagination range should change"""
+
         pagination = make_pagination_range(
             page_range=list(range(1, 21)),
             page_quantity=4,
             page_current=1,
         )
         self.assertEqual([1, 2, 3, 4], pagination)
+
+        pagination = make_pagination_range(
+            page_range=list(range(1, 21)),
+            page_quantity=4,
+            page_current=2,
+        )
+        self.assertEqual([1, 2, 3, 4], pagination)
+
+        pagination = make_pagination_range(
+            page_range=list(range(1, 21)),
+            page_quantity=4,
+            page_current=3,
+        )
+        self.assertEqual([2, 3, 4, 5], pagination)
+
+        pagination = make_pagination_range(
+            page_range=list(range(1, 21)),
+            page_quantity=4,
+            page_current=4,
+        )
+        self.assertEqual([3, 4, 5, 6], pagination)
--- utils/pagination.py
+++ utils/pagination.py
@@ -1,2 +1,13 @@
+import math
+
+
 def make_pagination_range(page_range, page_quantity, page_current):
-    return [1, 2, 3, 4]
+    middle_range = math.ceil(page_quantity / 2)
+    start_range = 0
+    stop_range = page_quantity
+
+    if page_current > middle_range:
+        start_range = page_current - middle_range
+        stop_range = page_current + middle_range
+
+    return page_range[start_range: stop_range]
