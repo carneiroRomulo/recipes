d8f9be15f29cc41363a239a9c518ebb17ccf5ee8
Romulo Carneiro <romulo.carneirodk@gmail.com>
2022-06-22 09:35:03 -0300

    refactor: separete pagination tests

--- recipes/tests/test_pagination.py
+++ recipes/tests/test_pagination.py
@@ -4,34 +4,54 @@ from utils.pagination import make_pagination_range
 
 
 class PaginationTest(TestCase):
-    def test_make_pagination_range_returns_correct_pagination_range(self):
-        """When current page is bigger than (page_quantity / 2)
-        pagination range should change"""
-
+    def test_make_pagination_range_is_static_near_the_start(self):
         pagination = make_pagination_range(
             page_range=list(range(1, 21)),
             page_quantity=4,
             page_current=1,
-        )
+        )['pagination']
         self.assertEqual([1, 2, 3, 4], pagination)
 
         pagination = make_pagination_range(
             page_range=list(range(1, 21)),
             page_quantity=4,
             page_current=2,
-        )
+        )['pagination']
         self.assertEqual([1, 2, 3, 4], pagination)
+
+    def test_make_pagination_range_is_static_near_the_end(self):
+        pagination = make_pagination_range(
+            page_range=list(range(1, 21)),
+            page_quantity=4,
+            page_current=18,
+        )['pagination']
+        self.assertEqual([17, 18, 19, 20], pagination)
 
         pagination = make_pagination_range(
             page_range=list(range(1, 21)),
             page_quantity=4,
+            page_current=20,
+        )['pagination']
+        self.assertEqual([17, 18, 19, 20], pagination)
+
+    def test_make_pagination_range_returns_correct_pagination_range(self):
+        pagination = make_pagination_range(
+            page_range=list(range(1, 21)),
+            page_quantity=4,
             page_current=3,
-        )
+        )['pagination']
         self.assertEqual([2, 3, 4, 5], pagination)
 
         pagination = make_pagination_range(
             page_range=list(range(1, 21)),
             page_quantity=4,
-            page_current=4,
-        )
-        self.assertEqual([3, 4, 5, 6], pagination)
+            page_current=13,
+        )['pagination']
+        self.assertEqual([12, 13, 14, 15], pagination)
+
+        pagination = make_pagination_range(
+            page_range=list(range(1, 21)),
+            page_quantity=4,
+            page_current=17,
+        )['pagination']
+        self.assertEqual([16, 17, 18, 19], pagination)
--- utils/pagination.py
+++ utils/pagination.py
@@ -3,11 +3,26 @@ import math
 
 def make_pagination_range(page_range, page_quantity, page_current):
     middle_range = math.ceil(page_quantity / 2)
+    # Static page range near the first pages
     start_range = 0
     stop_range = page_quantity
 
     if page_current > middle_range:
-        start_range = page_current - middle_range
-        stop_range = page_current + middle_range
+        if page_current < len(page_range) - middle_range:
+            # Dynamic page range
+            start_range = page_current - middle_range
+            stop_range = page_current + middle_range
+        else:
+            # Static page range near the last pages
+            start_range = len(page_range) - page_quantity
+            stop_range = len(page_range)
 
-    return page_range[start_range: stop_range]
+    pagination = page_range[start_range: stop_range]
+    return {
+        "pagination": pagination,
+        "page_current": page_current,
+        "page_range": page_range,
+        "first_page_out_of_range": page_current > middle_range,
+        "last_page_out_of_range":
+            page_current < len(page_range) - middle_range,
+    }
