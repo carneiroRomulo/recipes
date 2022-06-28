34b5c50ae6cf7df3ee8f7bc34cad7681b00e2902
Romulo Carneiro <romulo.carneirodk@gmail.com>
2022-06-28 12:19:44 -0300

    style: form

--- authors/views.py
+++ authors/views.py
@@ -1,5 +1,22 @@
-from django.shortcuts import render
+from django.http import Http404
+from django.shortcuts import redirect, render
+
+from .forms import RegisterForm
 
 
 def register_view(request):
-    return render(request, 'authors/pages/register_view.html')
+    register_form_data = request.session.get('register_form_data', None)
+    form = RegisterForm(register_form_data)
+    return render(request, 'authors/pages/register_view.html', {
+        'form': form,
+    })
+
+
+def register_create(request):
+    if not request.POST:
+        raise Http404()
+
+    request.session['register_form_data'] = request.POST
+    form = RegisterForm(request.POST)
+
+    return redirect('authors:register')
