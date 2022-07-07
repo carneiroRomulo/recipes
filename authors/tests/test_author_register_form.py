f79e681357383089f5272a9d307f93529e03c56f
Romulo Carneiro <romulo.carneirodk@gmail.com>
2022-07-07 13:21:24 -0300

    tests: test_author_register_form

---
+++ authors/tests/test_author_register_form.py
@@ -0,0 +1,40 @@
+from authors.forms import RegisterForm
+from django.test import TestCase
+from parameterized import parameterized
+
+
+class AuthorRegisterFormUnitTest(TestCase):
+    @parameterized.expand([
+        ('first_name',          'Ex.: Romulo'),
+        ('last_name',           'Ex.: Carneiro'),
+        ('username',            'Your username'),
+        ('email',               'Your e-mail'),
+        ('password',            'Your password'),
+        ('confirm_password',    'Confirm password'),
+    ])
+    def test_fields_placeholder_is_correct(self, field, needed_field):
+        form = RegisterForm()
+        current_field = form[field].field.widget.attrs['placeholder']
+        self.assertEqual(current_field, needed_field)
+
+    @parameterized.expand([
+        ('first_name', 'Type only letters'),
+        ('last_name',  'Type only letters'),
+    ])
+    def test_fields_help_text_is_correct(self, field, needed_field):
+        form = RegisterForm()
+        current_field = form[field].field.help_text
+        self.assertEqual(current_field, needed_field)
+
+    @parameterized.expand([
+        ('first_name',          'First Name'),
+        ('last_name',           'Last Name'),
+        ('username',            'Username'),
+        ('email',               'E-mail'),
+        ('password',            'Password'),
+        ('confirm_password',    'Confirm Password'),
+    ])
+    def test_fields_label_is_correct(self, field, needed_field):
+        form = RegisterForm()
+        current_field = form[field].field.label
+        self.assertEqual(current_field, needed_field)
