6e00185b65072ff520f8736e28a2e5eb85927001
Romulo Carneiro <romulo.carneirodk@gmail.com>
2022-07-11 21:51:02 -0300

    refactor: change static files directories

--- backend/settings.py
+++ backend/settings.py
@@ -110,6 +110,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'base_static',
+]
 STATIC_ROOT = BASE_DIR / 'static'
 
 MEDIA_URL = '/media/'
--- recipes/admin.py
+++ recipes/admin.py
@@ -1,10 +1,15 @@
 from django.contrib import admin
+
 from .models import Category, Recipe
+
 
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', )
-admin.site.register(Category, CategoryAdmin)
+
 
 class RecipeAdmin(admin.ModelAdmin):
     list_display = ('id', 'title')
+
+
+admin.site.register(Category, CategoryAdmin)
 admin.site.register(Recipe, RecipeAdmin)
