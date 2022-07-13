bd5b9db5b46ca3192dc519f2752325de565ca942
Romulo Carneiro <romulo.carneirodk@gmail.com>
2022-07-13 08:12:50 -0300

    build: update dependencies & conf selenium

---
+++ utils/browser.py
@@ -0,0 +1,27 @@
+from pathlib import Path
+from time import sleep
+
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+
+ROOT_PATH = Path(__file__).parent.parent
+CHROMEDRIVER_PATH = ROOT_PATH / 'chromedriver/stable/chromedriver'
+
+
+def make_chrome_browser(*options):
+    chrome_options = webdriver.ChromeOptions()
+    if options is not None:
+        for option in options:
+            chrome_options.add_argument(option)
+
+    chrome_service = Service(executable_path=str(CHROMEDRIVER_PATH))
+    browser = webdriver.Chrome(service=chrome_service, options=chrome_options)
+
+    return browser
+
+
+if __name__ == "__main__":
+    browser = make_chrome_browser("--headless", "--no-sandbox")
+    browser.get('https://www.google.com/')
+    sleep(5)
+    browser.quit()
