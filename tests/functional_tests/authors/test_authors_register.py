8c4e1872c426f8213fda6129c8996d2b69ddd78b
Romulo Carneiro <romulo.carneirodk@gmail.com>
2022-07-14 10:29:29 -0300

    test (functional): test_empty_first_name_error_message

---
+++ tests/functional_tests/authors/test_authors_register.py
@@ -0,0 +1,37 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+
+from .base import AuthorsBaseTest
+
+
+class AuthorsRegisterTest(AuthorsBaseTest):
+    def get_by_placeholder(self, web_element, placeholder):
+        return web_element.find_element(
+            By.XPATH, f'//input[@placeholder="{placeholder}"]')
+
+    def fill_form_dummy_data(self, form):
+        fields = form.find_elements(By.TAG_NAME, 'input')
+        for field in fields:
+            if field.is_displayed():
+                field.send_keys(' ' * 20)
+
+    def test_empty_first_name_error_message(self):
+        self.browser.get(self.live_server_url + '/authors/register/')
+        form = self.browser.find_element(
+            By.XPATH,
+            '/html/body/main/div[2]/form'
+        )
+
+        self.fill_form_dummy_data(form)
+        form.find_element(By.NAME, 'email').send_keys('test@test.com')
+
+        first_name_field = self.get_by_placeholder(form, 'Ex.: Romulo')
+        first_name_field.send_keys(' ')
+        first_name_field.send_keys(Keys.ENTER)
+
+        # Check if error is showed
+        error = self.browser.find_element(
+            By.XPATH,
+            '/html/body/main/div[2]/form/div[1]/div[1]/ul'
+        )
+        self.assertIn('This field is required', error.text)
